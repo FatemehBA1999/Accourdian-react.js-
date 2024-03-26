eeae8f728e569c61b42c3fa82ab3b203d7c680cc
fatemeh <fatemeh211100@gmail.com>
2024-03-26 13:56:18 +0330

    last commit

--- src/Components/Accordion.jsx
+++ src/Components/Accordion.jsx
@@ -18,31 +18,39 @@ const data = [
   },
 ];
 function Accordion() {
+  const [open, setOpen] = useState(null);
   return (
     <div className="accordion">
       {data.map((item) => (
-        <AcordionItem item={item} key={item.id} />
+        <AcordionItem item={item} key={item.id} setOpen={setOpen} open={open} />
       ))}
     </div>
   );
 }
 
 export default Accordion;
-function AcordionItem({ item }) {
-  const [isOpen, setIsOpen] = useState(false);
+function AcordionItem({ item, setOpen, open }) {
+  // const [isOpen, setIsOpen] = useState(false);
+  // a state undependency of other state
+
+  // all state are dependency || it is a state give id => 1,2,3,....
   const style = {
     width: "1.2rem",
     transition: "all 0.2s ease-out",
   };
+  // const isOpen = item.id === open ? true : false; // this or below
+  const isOpen = item.id === open;
+
   return (
     <div className={`accordion-item ${isOpen ? "accordion__expanded" : ""}`}>
       <div
         className="accordion-item__header"
-        onClick={() => setIsOpen((isOpen) => !isOpen)}
+        // onClick={() => setIsOpen((isOpen) => !isOpen)}
+        onClick={() => setOpen(item.id === open ? null : item.id)}
       >
         <div>{item.title}</div>
-        <ChevronDownIcon 
-        className="accordion-item__chevron"
+        <ChevronDownIcon
+          className="accordion-item__chevron"
           // style={{
           //   width: "1.2rem",
           //   transition: "all 0.6s ease-in-out",
